5c2b28cf4e61b03fbcc39cd5e5969f9bcff97157
Jered Masters <jered.masters@outlook.com>
2024-04-25 01:02:33 +0800

    fix appspec.yml

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,12 +3,14 @@ npm install
 
 #!/bin/bash
 
-# Install node.js
-sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:chris-lea/node.js -y
-sudo apt-get update
-sudo apt-get install nodejs -y
+sudo yum update
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install --lts
 # Install nodemon
 # sudo npm install nodemon -g
 
