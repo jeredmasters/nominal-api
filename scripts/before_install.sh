c6d9706607e05bbfc700d278b90ce7b29a7632cd
Jered Masters <jered.masters@outlook.com>
2024-04-25 01:52:27 +0800

    fix appspec.yml

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,21 +1,17 @@
-npm install
 
 
 #!/bin/bash
 
+# sudo yum update
+
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# nvm install --lts
+
 sudo yum update
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-nvm install --lts
-# Install nodemon
-# sudo npm install nodemon -g
-
-# Install forever module 
-# https://www.npmjs.com/package/forever
-
-# Clean working folder
-# sudo find /home/ubuntu/test -type f -delete
+sudo yum install nodejs-legacy -y
+sudo yum install npm  -y
+sudo npm install pm2 -g
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,8 @@
+cd /var/app
+
 npm install
 npm run migrations
 npm run start
+
+pm2 stop all
+pm2 start /var/app
